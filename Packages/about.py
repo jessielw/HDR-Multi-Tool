e63c9b3b87a0e62af6884809403feefc9c35c2db
jlw4049 <jlw_4049@hotmail.com>
2020-11-07 21:54:23 -0500

    -

---
+++ Packages/about.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+def openaboutwindow():  # About Window Information
+    about_window = Toplevel()
+    about_window.title('About')
+    about_window.configure(background="#434547")
+    window_height = 140
+    window_width = 470
+    screen_width = about_window.winfo_screenwidth()
+    screen_height = about_window.winfo_screenheight()
+    x_cordinate = int((screen_width / 2) - (window_width / 2))
+    y_cordinate = int((screen_height / 2) - (window_height / 2))
+    about_window.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+    about_window_text = Text(about_window, background="#434547", foreground="white", relief=SUNKEN)
+    about_window_text.pack()
+    about_window_text.configure(state=NORMAL)
+    about_window_text.insert(INSERT, "HDR10PlusParser \n")
+    about_window_text.insert(INSERT, "\n")
+    about_window_text.insert(INSERT, "Development: jlw4049")
+    about_window_text.insert(INSERT, "\n\n")
+    about_window_text.insert(INSERT, "A basic GUI wrapper for hdr10plus_parser. \n")
+    about_window_text.configure(state=DISABLED)
