117bb550e6ebab4ffdb3c287c90e42c1a44e0968
jlw4049 <jlw_4049@hotmail.com>
2020-11-08 22:51:38 -0500

    - Fixed Typo and organized messagebox alignment

--- HDR10PlusParser.py
+++ HDR10PlusParser.py
@@ -13,8 +13,8 @@ from Packages.about import openaboutwindow
 
 # Main Gui & Windows --------------------------------------------------------
 def root_exit_function():  # Asks if user wants to close main GUI + close all tasks
-    confirm_exit = messagebox.askyesno(title='Prompt', message="Are you sure you want to exit the program?\nThis "
-                                                               "will end all current taks.",
+    confirm_exit = messagebox.askyesno(title='Prompt', message="Are you sure you want to exit the program?\n\n"
+                                                               "     Note: This will end all current tasks!",
                                        parent=root)
     if confirm_exit == False:
         pass
