b61e0650ce6217099196d587eaa7826cd29fa29a
jlw4049 <jlw_4049@hotmail.com>
2023-09-16 00:37:41 -0400

    feat: allow ipcrenderer to be used by the renderer script

---
+++ electron/app/preload/preload.js
@@ -0,0 +1,8 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("ipcRenderer", {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  on: (channel, func) =>
+    ipcRenderer.on(channel, (event, ...args) => func(...args)),
+  invoke: (...args) => ipcRenderer.invoke(...args),
+});
