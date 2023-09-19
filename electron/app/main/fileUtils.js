e6a344c188b547de87dbac9b7e129d9f48911127
jlw4049 <jlw_4049@hotmail.com>
2023-09-19 13:53:00 -0400

    fix: ensure that full normalized paths are returned for each OS

--- electron/app/main/fileUtils.js
+++ electron/app/main/fileUtils.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 const getPathObject = function (file) {
   return {
-    dirName: path.dirname(file),
-    path: path.resolve(file),
+    dirName: path.normalize(path.dirname(file)),
+    path: path.normalize(path.resolve(file)),
     baseName: path.basename(file),
     ext: path.extname(file),
   };
@@ -13,8 +13,8 @@ const changeFileExtension = function (filePath, ext) {
   const basename = path.basename(filePath, path.extname(filePath));
   const modifiedPath = path.join(path.dirname(filePath), basename + ext);
   return {
-    dirName: path.dirname(modifiedPath),
-    path: path.resolve(modifiedPath),
+    dirName: path.normalize(path.dirname(modifiedPath)),
+    path: path.normalize(path.resolve(modifiedPath)),
     baseName: path.basename(modifiedPath),
     ext: path.extname(modifiedPath),
   };
